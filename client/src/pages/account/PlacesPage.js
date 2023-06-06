4ccab7ac48e15245c542de85dc60b264e580b612
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-06-06 18:36:21 +0600

    client side full customize with routing setup

--- client/src/pages/account/PlacesPage.js
+++ client/src/pages/account/PlacesPage.js
@@ -2,78 +2,20 @@ import React, { useContext, useState } from "react";
 import { UserContext } from "../../context/UserContext";
 import { Navigate, Link, useParams } from "react-router-dom";
 import axios from "axios";
-import Place from "../../components/places/Place";
+import AccountNav from "../../components/navbar/AccountNav";
+import { Button } from "react-bootstrap";
 
 const PlacesPage = () => {
-  const [redirect, setRedirect] = useState(null);
-  const { user, ready, setUser } = useContext(UserContext);
-
-  let { subpage } = useParams();
-
-  if (!ready) {
-    return <h1>Loading...</h1>;
-  }
-
-  if (ready && !user && !redirect) {
-    return <Navigate to={`/login`} />;
-  }
-
-  if (subpage === undefined) {
-    subpage = "profile";
-  }
-
-  function linkClasses(type = null) {
-    let classes = `px-3`;
-    if (type === subpage) {
-      classes += " p-2 bg-primary text-light";
-    } else {
-      classes += " p-2 bg-light text-black";
-    }
-
-    return classes;
-  }
-
-  //   logout user
-  const logoutUser = async () => {
-    try {
-      await axios.post(`/logout`);
-      alert(`User logout`);
-      setRedirect("/");
-      setUser(null);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  if (redirect) {
-    return <Navigate to={redirect} />;
-  }
-
   return (
     <>
-      <div className="mx-auto w-75 text-center mt-4">
-        <Link to={`/account`} className={linkClasses("profile")}>
-          My Profile
+      <AccountNav />
+      <div className="text-center mt-4">
+        <Link to={`/account/places/new`}>
+          <Button variant="secondary" className="btn-sm btn">
+            + Add New Place
+          </Button>
         </Link>
-        <Link to={`/account/bookings`} className={linkClasses("bookings")}>
-          My Bookings
-        </Link>
-        <Link to={`/account/places`} className={linkClasses(`places`)}>
-          My Accommodation
-        </Link>
-
-        {subpage === "profile" && (
-          <div>
-            <p className="py-3">
-              Logged in as Name: {user.name} Email: ({user.email})
-            </p>
-            <button className="btn btn-danger" onClick={logoutUser}>
-              Logout
-            </button>
-          </div>
-        )}
       </div>
-      {subpage === "places" && <Place />}
     </>
   );
 };
--- client/src/pages/placesFormPage/PlacesFormPage.js
+++ client/src/pages/placesFormPage/PlacesFormPage.js
@@ -1,11 +1,150 @@
-import React from 'react'
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import { Link, useParams } from "react-router-dom";
+import { FaCloudUploadAlt } from "react-icons/fa";
+import axios from "axios";
+import UploadPhotos from "../../components/uploadPhotos/UploadPhotos";
+import Perks from "../../components/perks/Perks";
+import AccountNav from "../../components/navbar/AccountNav";
 
-const PlacesFormPage = () => {
+const Place = () => {
+  const { action } = useParams();
+
+  // state for inputs
+  const [title, setTitle] = useState("");
+  const [address, setAddress] = useState("");
+  const [addedPhotos, setAddedPhotos] = useState([]);
+  const [description, setDescription] = useState("");
+  const [perks, setPerks] = useState("");
+  const [extraInfo, setExtraInfo] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [maxGuests, setMaxGuests] = useState(1);
+
+  function inputHeader(text) {
+    return <h4 className="mt-4">{text}</h4>;
+  }
+
+  function inputDescription(text) {
+    return <p className="text-secondary">{text}</p>;
+  }
+
+  function preInput(header, description) {
+    return (
+      <>
+        {inputHeader(header)}
+        {inputDescription(description)}
+      </>
+    );
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      Places form page
-    </div>
-  )
-}
+    <>
+      <div className="mt-5">
+        <AccountNav />
 
-export default PlacesFormPage
+        <div className="container">
+          <div className="col-6 mx-auto">
+            <div className="py-5">
+              <form onSubmit={handleSubmit}>
+                <div>
+                  {preInput(
+                    `Title`,
+                    ` Title for your place, should be short and catchy as in
+              advertisement`
+                  )}
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="title, for example: My lovely apartment"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+
+                {preInput(`Address`, ` Address to this place`)}
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="address"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+
+                {preInput(`Photos`, ` more = better`)}
+
+                {/* UPLOAD PHOTOS */}
+                <UploadPhotos
+                  addedPhotos={addedPhotos}
+                  setAddedPhotos={setAddedPhotos}
+                />
+
+                {preInput(`Description`, ` description of this place`)}
+
+                <textarea
+                  className="form-control"
+                  type="text"
+                  placeholder="description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+
+                {preInput(`Perks`, `select all the perks of your place`)}
+
+                {/* perks component */}
+                <Perks value={perks} setPerks={setPerks} />
+
+                {preInput(`Extra Info`, `house rules, etc`)}
+                <textarea
+                  className="form-control"
+                  type="text"
+                  value={extraInfo}
+                  onChange={(e) => setExtraInfo(e.target.value)}
+                />
+
+                {preInput(`Checkin & Out times`, `add check in and out times`)}
+                <h4>Check in time</h4>
+                <input
+                  className="form-control"
+                  type="number"
+                  placeholder="14:00"
+                  value={checkIn}
+                  onChange={(e) => setCheckIn(e.target.value)}
+                />
+
+                <h4 className="my-3">Check out time</h4>
+                <input
+                  className="form-control"
+                  type="number"
+                  placeholder="16:00"
+                  value={checkOut}
+                  onChange={(e) => setCheckOut(e.target.value)}
+                />
+
+                <h4>Max guest</h4>
+                <input
+                  className="form-control"
+                  type="number"
+                  value={maxGuests}
+                  onChange={(e) => setMaxGuests(e.target.value)}
+                />
+
+                <div className="d-grid gap-2 mt-3">
+                  <Button variant="primary" size="lg">
+                    Save
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Place;
